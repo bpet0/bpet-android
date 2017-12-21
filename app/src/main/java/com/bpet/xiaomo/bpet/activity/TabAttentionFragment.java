59dc63f6c7c466f03d7cc00740e2e5e5af2b6d72
xiaomo <hcxxiaomo@163.com>
2017-12-21 22:41:42 +0800

    删除原来的list，准备使用MaterialViewPager

--- app/src/main/java/com/bpet/xiaomo/bpet/activity/TabAttentionFragment.java
+++ app/src/main/java/com/bpet/xiaomo/bpet/activity/TabAttentionFragment.java
@@ -13,9 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.bpet.xiaomo.bpet.R;
-import com.daimajia.androidanimations.library.Techniques;
-import com.daimajia.androidanimations.library.YoYo;
-import com.daimajia.androidviewhover.BlurLayout;
+
 
 /**
  * Created by Stefan on 17/12/13.
@@ -28,7 +26,7 @@ public final class TabAttentionFragment extends Fragment {
     private Context mContext;
     private View newsLayout;
 
-    private BlurLayout mSampleLayout, mSampleLayout2, mSampleLayout3, mSampleLayout4;
+    //private BlurLayout mSampleLayout, mSampleLayout2, mSampleLayout3, mSampleLayout4;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -36,102 +34,8 @@ public final class TabAttentionFragment extends Fragment {
         mContext = getActivity();
         newsLayout = inflater.inflate(R.layout.fragment_attention, container,
                 false);
-        newsLayout.setPadding(0,0,0,80);
-        BlurLayout.setGlobalDefaultDuration(450);
-        mSampleLayout = (BlurLayout)newsLayout.findViewById(R.id.blur_layout);
-        View hover = LayoutInflater.from(mContext).inflate(R.layout.hover_sample, null);
-        hover.findViewById(R.id.heart).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                YoYo.with(Techniques.Tada)
-                        .duration(550)
-                        .playOn(v);
-            }
-        });
-        hover.findViewById(R.id.share).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                YoYo.with(Techniques.Swing)
-                        .duration(550)
-                        .playOn(v);
-            }
-        });
-        mSampleLayout.setHoverView(hover);
-        mSampleLayout.setBlurDuration(550);
-        mSampleLayout.addChildAppearAnimator(hover, R.id.heart, Techniques.FlipInX, 550, 0);
-        mSampleLayout.addChildAppearAnimator(hover, R.id.share, Techniques.FlipInX, 550, 250);
-        mSampleLayout.addChildAppearAnimator(hover, R.id.more, Techniques.FlipInX, 550, 500);
+        //newsLayout.setPadding(0,0,0,80);
 
-        mSampleLayout.addChildDisappearAnimator(hover, R.id.heart, Techniques.FlipOutX, 550, 500);
-        mSampleLayout.addChildDisappearAnimator(hover, R.id.share, Techniques.FlipOutX, 550, 250);
-        mSampleLayout.addChildDisappearAnimator(hover, R.id.more, Techniques.FlipOutX, 550, 0);
-
-        mSampleLayout.addChildAppearAnimator(hover, R.id.description, Techniques.FadeInUp);
-        mSampleLayout.addChildDisappearAnimator(hover, R.id.description, Techniques.FadeOutDown);
-
-        //sample 2
-
-        mSampleLayout2 = (BlurLayout)newsLayout.findViewById(R.id.blur_layout2);
-        View hover2 = LayoutInflater.from(mContext).inflate(R.layout.hover_sample2, null);
-        hover2.findViewById(R.id.avatar).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(mContext, "Pretty Cool, Right?", Toast.LENGTH_SHORT).show();
-            }
-        });
-        mSampleLayout2.setHoverView(hover2);
-
-        mSampleLayout2.addChildAppearAnimator(hover2, R.id.description, Techniques.FadeInUp);
-        mSampleLayout2.addChildDisappearAnimator(hover2, R.id.description, Techniques.FadeOutDown);
-        mSampleLayout2.addChildAppearAnimator(hover2, R.id.avatar, Techniques.DropOut, 1200);
-        mSampleLayout2.addChildDisappearAnimator(hover2, R.id.avatar, Techniques.FadeOutUp);
-        mSampleLayout2.setBlurDuration(1000);
-
-        //sample3
-        mSampleLayout3 = (BlurLayout)newsLayout.findViewById(R.id.blur_layout3);
-        View hover3 = LayoutInflater.from(mContext).inflate(R.layout.hover_sample3, null);
-        mSampleLayout3.setHoverView(hover3);
-        mSampleLayout3.addChildAppearAnimator(hover3, R.id.eye, Techniques.Landing);
-        mSampleLayout3.addChildDisappearAnimator(hover3, R.id.eye, Techniques.TakingOff);
-        mSampleLayout3.enableZoomBackground(true);
-        mSampleLayout3.setBlurDuration(1200);
-
-        //sample 4
-
-        mSampleLayout4 = (BlurLayout)newsLayout.findViewById(R.id.blur_layout4);
-        View hover4 = LayoutInflater.from(mContext).inflate(R.layout.hover_sample4,null);
-        mSampleLayout4.setHoverView(hover4);
-        mSampleLayout4.addChildAppearAnimator(hover4, R.id.cat, Techniques.SlideInLeft);
-        mSampleLayout4.addChildAppearAnimator(hover4, R.id.mail, Techniques.SlideInRight);
-
-        mSampleLayout4.addChildDisappearAnimator(hover4, R.id.cat, Techniques.SlideOutLeft);
-        mSampleLayout4.addChildDisappearAnimator(hover4, R.id.mail, Techniques.SlideOutRight);
-
-        mSampleLayout4.addChildAppearAnimator(hover4, R.id.content, Techniques.BounceIn);
-        mSampleLayout4.addChildDisappearAnimator(hover4, R.id.content, Techniques.FadeOutUp);
-
-
-        hover4.findViewById(R.id.cat).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent getWebPage = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/daimajia"));
-                startActivity(getWebPage);
-            }
-        });
-
-        hover4.findViewById(R.id.mail).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-
-                emailIntent.setType("plain/text");
-                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"daimajia@gmail.com"});
-                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "About AndroidViewHover");
-                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "I have a good idea about this project..");
-
-                startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-            }
-        });
 
         return newsLayout;
     }
